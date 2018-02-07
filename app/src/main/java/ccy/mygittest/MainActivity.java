d37a5068393d83a3835399c19cf09e336c73679a
chenchaoyong <671518768@qq.com>
2018-02-07 15:42:44 +0800

    b2

--- app/src/main/java/ccy/mygittest/MainActivity.java
+++ app/src/main/java/ccy/mygittest/MainActivity.java
@@ -18,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         String b1 = "分支1";
+        String b2="asd";
     }
 
     @Override
