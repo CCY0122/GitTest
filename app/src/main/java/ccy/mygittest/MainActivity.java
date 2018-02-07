fc17667daff5f9623783bd796ae76c4f0cdbfb4e
chenchaoyong <671518768@qq.com>
2018-02-07 11:31:37 +0800

    all

--- app/src/main/java/ccy/mygittest/MainActivity.java
+++ app/src/main/java/ccy/mygittest/MainActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 
     String a;
+    String b;
+    String c;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
