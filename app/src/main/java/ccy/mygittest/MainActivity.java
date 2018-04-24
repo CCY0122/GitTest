25f837ef293cae3b2e43d138b4eae1fd8a15ba28
chenchaoyong <671518768@qq.com>
2018-04-24 15:23:06 +0800

    主分支做了修改

--- app/src/main/java/ccy/mygittest/MainActivity.java
+++ app/src/main/java/ccy/mygittest/MainActivity.java
@@ -26,6 +26,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
+        //this is master modified;
 
     }
 
---
+++ app/src/main/java/ccy/mygittest/Test.java
@@ -0,0 +1,12 @@
+package ccy.mygittest;
+
+/**
+ * Created by chenchaoyong on 2018/4/24.
+ */
+
+public class Test {
+    //this class created by master
+    public void empty(){
+
+    }
+}
