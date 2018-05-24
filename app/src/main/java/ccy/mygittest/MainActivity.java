261c2035be4f496fec38ebf3cd6160ffa7d6e232
chenchaoyong <671518768@qq.com>
2018-05-24 11:35:38 +0800

    qq

--- app/src/main/java/ccy/mygittest/MainActivity.java
+++ app/src/main/java/ccy/mygittest/MainActivity.java
@@ -22,7 +22,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onRestart() {
         super.onRestart();
-        //this is macbrance1 modified;
+        //this is macbrance1 modifbbbbbied;
         //
     }
 
